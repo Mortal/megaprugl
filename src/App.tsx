d8a48a64bbcade774525c67465a34ee7cdd85347
Mathias Rav <m@git.strova.dk>
2022-04-02 19:23:19 +0200

    Big ass WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,169 +1,229 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import * as Game from './Game';
+import './App.css';
+import { onSnapshot } from "mobx-state-tree";
+import { observer } from "mobx-react-lite";
+import { cards } from './Cards';
 
 const useEventListener = <K extends keyof ElementEventMap>(
-  target: Element, event: K,
-  cb: (this: Element, ev: ElementEventMap[K]) => any,
-  options?: boolean | AddEventListenerOptions
+    target: Element, event: K,
+    cb: (this: Element, ev: ElementEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions
 ) => {
-  React.useEffect(() => {
-    target.addEventListener(event, cb, options);
-    return () => target.removeEventListener(event, cb, options);
-  }, [cb]);
+    React.useEffect(() => {
+        target.addEventListener(event, cb, options);
+        return () => target.removeEventListener(event, cb, options);
+    }, [cb]);
 };
 
 const useWindowEventListener = <K extends keyof WindowEventMap>(
-  event: K,
-  cb: (this: Window, ev: WindowEventMap[K]) => any,
-  options?: boolean | AddEventListenerOptions
+    event: K,
+    cb: (this: Window, ev: WindowEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions
 ) => {
-  useEventListener(window as unknown as any, event as unknown as any, cb as unknown as any, options);
+    useEventListener(window as unknown as any, event as unknown as any, cb as unknown as any, options);
+};
+
+const Card: React.FC<{value: number}> = ({value}) => {
+    return <div style={{display: "inline-flex", width: "100px"}}>{cards[value]}</div>
 };
 
 const Cards: React.FC<{coords: (readonly [number, number])[], onClick: () => void}> =
 ({coords, onClick}) => {
-  return <>
-    {coords.map(([x, y], i) => <div style={{position: "absolute", left: `${x}px`, top: `${y}px`, display: "flex"}} onClick={onClick} key={i}>{i}</div>)}
-  </>;
+    const w = 100;
+    return <>
+        {coords.map(([x, y], i) =>
+        <div style={{
+            position: "absolute",
+            left: `${x-w/2}px`,
+            top: `${y-w*1.5/2}px`,
+            display: "flex",
+            width: `${w}px`,
+            height: `${w*1.5}px`,
+        }} onClick={onClick} key={i}>
+            {cards[55]}
+        </div>)}
+    </>;
 }
 
 const getMouseXy = (ev: MouseEvent) => {
-  return [ev.clientX, ev.clientY] as const;
+    return [ev.clientX, ev.clientY] as const;
 };
 
 const useMouseMove = (pos: readonly [number, number], setPos: (p: readonly [number, number]) => void) => {
-  const [x, y] = pos;
-  const [[dx, dy], setD] = React.useState([0 as number, 0 as number] as const);
-  const [s, setS] = React.useState<null | readonly [number, number]>(null);
-  const onMouseDown = React.useCallback((ev: MouseEvent) => {
-    setS(getMouseXy(ev));
-  }, []);
-  const onMouseUp = React.useCallback((ev: MouseEvent) => {
-    if (s == null) return;
-    setS(null);
-    setD([0, 0]);
-    setPos([x + dx, y + dy]);
-  }, [x, y, dx, dy, s, setPos]);
-  const onMouseMove = React.useCallback((ev: MouseEvent) => {
-    if (s == null) return;
-    const [xx, yy] = getMouseXy(ev);
-    const [sx, sy] = s;
-    setD([xx - sx, yy - sy]);
-  }, [s]);
-  return [x + dx, y + dy, onMouseDown, onMouseMove, onMouseUp] as const;
-}
-
-const Player: React.FC<{value: Player, index: number, onClick?: (i: number) => void, update: (i: number, f: (p: Player) => Player) => void}> = ({value: p, onClick, index, update}) => {
-  const [x, y, onMouseDown, onMouseMove, onMouseUp] = useMouseMove([p.x, p.y], ([x, y]) => update(index, (p) => ({...p, x, y})));
-  useWindowEventListener("mousemove", onMouseMove);
-  useWindowEventListener("mouseup", onMouseUp);
-  const [confirm, setConfirm] = React.useState(false);
-  React.useEffect(() => {
-    if (onClick != null) setConfirm(false);
-  }, [onClick]);
-  return <div
-    style={{
-      position: "absolute",
-      left: `${x}px`,
-      top: `${y}px`,
-      display: "flex",
-      flexDirection: "column",
-      border: "1px solid black",
-      background: "white",
-    }}
-    onMouseDown={onClick == null ? (e) => onMouseDown(e.nativeEvent) : undefined}
-    onClick={onClick == null ? undefined : () => onClick(index)}
-  >
-    <input value={p.name} onChange={(e) => update(index, (p) => ({...p, name: e.target.value}))} />
-    <div style={{display: "flex"}}>{(p.cards.length === 0 ? ["No cards"] : p.cards.map(c => "" + c)).map((c, i) => <div key={i} style={{margin: "5px"}}>{c}</div>)}</div>
-    <button disabled={onClick != null} onClick={() => confirm ? update(index, (p) => ({...p, hidden: true})) : setConfirm(true)}>{confirm ? "Really DNF?" : "DNF"}</button>
-    <button disabled={onClick != null} onClick={() => update(index, (p) => ({...p, cards: []}))}>Clear cards</button>
-  </div>;
+    const [x, y] = pos;
+    const [[dx, dy], setD] = React.useState([0 as number, 0 as number] as const);
+    const [s, setS] = React.useState<null | readonly [number, number]>(null);
+    const onMouseDown = React.useCallback((ev: MouseEvent) => {
+        setS(getMouseXy(ev));
+        ev.stopPropagation();
+    }, []);
+    const onMouseUp = React.useCallback((ev: MouseEvent) => {
+        if (s == null) return;
+        setS(null);
+        setD([0, 0]);
+        setPos([x + dx, y + dy]);
+    }, [x, y, dx, dy, s, setPos]);
+    const onMouseMove = React.useCallback((ev: MouseEvent) => {
+        if (s == null) return;
+        const [xx, yy] = getMouseXy(ev);
+        const [sx, sy] = s;
+        setD([xx - sx, yy - sy]);
+    }, [s]);
+    return [x + dx, y + dy, onMouseDown, onMouseMove, onMouseUp] as const;
 };
 
-const Players: React.FC<{value: Player[], onClick?: (i: number) => void, update: (i: number, f: (p: Player) => Player) => void}> = ({value, ...props}) => {
-  return <>
-    {value.map((p, i) => p.hidden ? null : <Player key={i} value={p} index={i} {...props} />)}
-  </>;
+const DirtyInput: React.FC<{value: string, onChange: (v: string) => void}> = ({value, onChange}) => {
+    const [dirtyValue, setDirtyValue] = React.useState<string | null>(null);
+    return <input
+        style={{width: "100%", boxSizing: 'border-box'}}
+        value={dirtyValue == null ? value : dirtyValue}
+        onChange={(e) => setDirtyValue(e.target.value === value ? null : e.target.value)}
+        onBlur={() => {if (dirtyValue != null) { onChange(dirtyValue); setDirtyValue(null); }}}
+        />;
 };
 
-interface Player {
-  name: string;
-  cards: number[];
-  x: number;
-  y: number;
-  hidden?: boolean;
-}
+const Player: React.FC<{
+    value: Game.Player,
+    index: number,
+    onClick?: (i: number) => void,
+}> = observer(({value: player, onClick, index}) => {
+    const [x, y, onMouseDown, onMouseMove, onMouseUp] = useMouseMove(player.position, (p) => player.move(p));
+    useWindowEventListener("mousemove", onMouseMove);
+    useWindowEventListener("mouseup", onMouseUp);
+    const [confirm, setConfirm] = React.useState(false);
+    React.useEffect(() => {
+        if (onClick != null) setConfirm(false);
+    }, [onClick]);
+    return <div
+        style={{
+            position: "absolute",
+            left: `${x-50}px`,
+            top: `${y-50}px`,
+            display: "flex",
+            flexDirection: "column",
+            border: "1px solid black",
+            background: "white",
+        }}
+        onMouseDown={onClick == null ? (e) => onMouseDown(e.nativeEvent) : undefined}
+        onClick={onClick == null ? undefined : () => onClick(index)}
+    >
+        <DirtyInput value={player.name} onChange={(s) => {console.log({s});player.setName(s)}} />
+        <div style={{display: "flex"}}>
+            {player.cards.length === 0 ? <div style={{margin: "5px"}}>No cards</div> : player.cards.map(
+                (c, i) => <div key={i} style={{margin: "5px", width: "40px"}}>{cards[c]}</div>
+            )}
+        </div>
+        <button disabled={onClick != null} onClick={() => confirm ? player.hide() : setConfirm(true)}>{confirm ? "Really DNF?" : "DNF"}</button>
+        <button disabled={onClick != null} onClick={() => player.clearCards()}>Clear cards</button>
+    </div>;
+});
 
-function App() {
-  const [mode, setMode] = React.useState("drag");
-  const coords = React.useRef<(readonly [number,number])[]>([]);
-  const [coordsLength, setCoordsLength] = React.useState(0);
-  const [drawn, setDrawn] = React.useState<number | null>(null);
-  const [players, setPlayers] = React.useState<Player[]>([]);
-  const drawTo = React.useCallback(([x, y]: readonly [number, number]) => {
-    coords.current.push([x, y]);
-    setCoordsLength(coords.current.length);
-  }, []);
-  useWindowEventListener("mousedown", React.useCallback((ev: MouseEvent) => {
-    if (ev.target instanceof Element && ev.target.tagName === "BUTTON") return;
-    if (mode === "drag") {
-      setMode("dragging");
-      coords.current = [];
-      drawTo(getMouseXy(ev));
-      ev.preventDefault();
-    }
-    if (mode === "placePlayer") {
-      const [x, y] = getMouseXy(ev);
-      setPlayers([...players, {name: "Player " + (players.length + 1), cards: [], x, y}]);
-      setMode("draw");
-    }
-  }, [mode]));
-  useWindowEventListener("mousemove", React.useCallback((ev: MouseEvent) => {
-    if (mode === "dragging") {
-      drawTo(getMouseXy(ev));
-    }
-  }, [mode]));
-  useWindowEventListener("mouseup", React.useCallback((ev: MouseEvent) => {
-    if (mode === "dragging") {
-      drawTo(getMouseXy(ev));
-      setMode("draw");
-    }
-  }, [mode]));
-  const drawCard = React.useCallback(() => {
-    setDrawn((Math.random() * 52)|0);
-    setMode("choose");
-  }, [])
-  const cardClick = React.useCallback(() => {
-    if (mode === "draw") {
-      drawCard();
-    }
-  }, [mode, drawCard]);
+const Players: React.FC<{
+    onClick?: (i: number) => void,
+}> = observer((props) => {
+    console.log("Hello I am observer Players");
+    const players = Game.useMst().game.players;
+    return <>
+        {players.map((p, i) => p.hidden ? null : <Player key={i} value={p} index={i} {...props} />)}
+    </>;
+});
 
-  return <>
-    <Cards coords={coords.current} onClick={cardClick} />
-    <Players
-      value={players}
-      update={(i, p) => setPlayers((ps) => [...ps.slice(0, i), p(ps[i]), ...ps.slice(i + 1)])}
-      onClick={mode !== "choose" ? undefined : (i) => {
-        if (drawn != null) {
-          setPlayers((ps) => [...ps.slice(0, i), {...ps[i], cards: [...ps[i].cards, drawn]}, ...ps.slice(i + 1)]);
-          setDrawn(null);
+const GameApp: React.FC<{}> = observer((_props) => {
+    const game = Game.useMst().game;
+    const [mode, setMode] = React.useState("game");
+    const isDragging = mode === "drag" || mode === "dragging" || (mode === "game" && game.cardPile.positions.length === 0);
+    useWindowEventListener("mousedown", React.useCallback((ev: MouseEvent) => {
+        if (ev.target instanceof Element && (ev.target.tagName === "BUTTON" || ev.target.tagName === "INPUT")) return;
+        if (isDragging) {
+            setMode("dragging");
+            game.restartPile(getMouseXy(ev));
+            ev.preventDefault();
+            ev.stopPropagation();
         }
-        setMode("draw");
-      }}
-      />
-    <div>Currently drawn card: {drawn}</div>
-    <div><button
-      onClick={() => setMode((m) => m === "drag" ? "draw" : "drag")}
-      disabled={!(mode === "drag" && coords.current.length > 0 || mode === "draw")}
-    >{(mode === "drag" || mode === "dragging") ? "Drag the new cards" : "Shuffle deck"}</button></div>
-    <div><button
-      onClick={() => setMode((m) => m === "draw" ? "placePlayer" : "draw")}
-      disabled={!(mode === "draw" || mode === "placePlayer")}
-    >{mode === "placePlayer" ? "Click the new player's location!" : "Add player"}</button></div>
-  </>;
-}
+        if (mode === "placePlayer") {
+            game.addPlayer(getMouseXy(ev));
+            setMode("game");
+            ev.preventDefault();
+            ev.stopPropagation();
+        }
+    }, [mode]));
+    useWindowEventListener("mousemove", React.useCallback((ev: MouseEvent) => {
+        if (mode === "dragging") {
+            game.pushPile(getMouseXy(ev));
+        }
+    }, [mode]));
+    useWindowEventListener("mouseup", React.useCallback((ev: MouseEvent) => {
+        if (mode === "dragging") {
+            game.pushPile(getMouseXy(ev));
+            setMode("game");
+        }
+    }, [mode]));
+    const drawCard = React.useCallback(() => {
+        game.drawCard((Math.random() * 52)|0);
+    }, []);
+    const drawnCard = game.drawnCard;
+    const cardClick = React.useCallback(() => {
+        if (drawnCard == null) {
+            drawCard();
+        }
+    }, [drawnCard, drawCard]);
 
-export default App
+    return <>
+        {mode !== "drag" && <Cards coords={[...game.cardPile.positions]} onClick={cardClick} />}
+        <Players
+            onClick={drawnCard == null ? undefined : (i) => {
+                game.giveCard(i);
+                setMode("game");
+            }}
+            />
+        <div><button
+            onClick={() => setMode((m) => m === "drag" ? "game" : "drag")}
+            disabled={!(mode === "drag" && game.cardPile.positions.length > 0 || mode === "game")}
+        >{isDragging ? "Deck shuffled - please drag to place the cards" : "Shuffle deck"}</button></div>
+        <div><button
+            onClick={() => setMode((m) => m === "game" ? "placePlayer" : "game")}
+            disabled={!(mode === "game" || mode === "placePlayer")}
+        >{mode === "placePlayer" ? "Click the new player's location!" : "Add player"}</button></div>
+        {drawnCard == null ? null : <div>
+            {"Currently drawn card: "}
+            <div style={{display: "inline-block", width: "100px", height: "140px"}}>
+                {cards[drawnCard]}
+            </div>
+        </div>}
+    </>;
+});
+
+const App: React.FC<{}> = (_props) => {
+    const rootRef = React.useRef<Game.RootInstance | null>(null);
+    if (rootRef.current == null) {
+        let gameData = {
+            game: {
+                cardPile: {},
+            },
+        };
+        const ls = window.localStorage.getItem("megaprugl");
+        if (ls != null) {
+            try {
+                const lsParsed = JSON.parse(ls);
+                if (Game.RootModel.is(lsParsed)) {
+                    gameData = lsParsed;
+                }
+            } catch {
+                // Ignore JSON.parse() errors
+            }
+        }
+        rootRef.current = Game.RootModel.create(gameData);
+    }
+    const root = rootRef.current;
+    React.useEffect(() => {
+        onSnapshot(root, (snapshot) => {
+            localStorage.setItem("megaprugl", JSON.stringify(snapshot));
+        })
+    }, [root]);
+    return <Game.Provider value={root}>
+        <GameApp />
+    </Game.Provider>;
+};
+
+export default App;
---
+++ src/Game.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Instance, types, onSnapshot } from "mobx-state-tree";
+
+export type Card = number;
+export const Card = types.number;
+
+export type Point2 = readonly [number, number];
+export const Point2 = types.custom<number[], Point2>({
+    name: "Point2",
+    fromSnapshot(value) { return [value[0], value[1]] as const; },
+    toSnapshot(value) { return [...value]; },
+    isTargetType(value) { return value != null && value.length === 2; },
+    getValidationMessage(value) { return value != null && value.length === 2 ? "" : "Wrong length"; },
+});
+
+export const PlayerModel = types.model("Player", {
+    name: types.string,
+    position: Point2,
+    cards: types.array(Card),
+    hidden: false,
+}).actions((self) => ({
+    setName(name: string) { self.name = name; },
+    clearCards() { self.cards.clear(); },
+    addCard(card: Card) { self.cards.push(card); },
+    move(newPosition: Point2) { self.position = newPosition; },
+    hide() { self.hidden = true; },
+}));
+export type Player = Instance<typeof PlayerModel>;
+
+export const CardPile = types.model("CardPile", {
+    positions: types.array(Point2),
+}).actions((self) => ({
+    restart(pos: Point2) {self.positions.clear(); self.positions.push(pos);},
+    push(pos: Point2) {self.positions.push(pos);},
+}));
+
+export const Game = types.model("Game", {
+    drawnCard: types.maybe(Card),
+    players: types.array(PlayerModel),
+    cardPile: CardPile,
+}).actions((self) => ({
+    drawCard(newCard: Card) {
+        if (self.drawnCard != undefined) return;
+        self.drawnCard = newCard;
+    },
+    giveCard(playerIndex: number) {
+        if (self.drawnCard == undefined) return;
+        self.players[playerIndex].addCard(self.drawnCard);
+        self.drawnCard = undefined;
+    },
+    restartPile(pos: Point2) { self.cardPile.restart(pos); },
+    pushPile(pos: Point2) { self.cardPile.push(pos); },
+    addPlayer(position: Point2, name?: string) {
+        self.players.push(PlayerModel.create({
+            name: name == null ? `Player ${self.players.length + 1}` : name,
+            position,
+        }));
+    },
+}));
+
+export const RootModel = types.model("Root", {
+    game: Game,
+});
+
+export type RootInstance = Instance<typeof RootModel>;
+const RootStoreContext = React.createContext<null | RootInstance>(null);
+export const Provider = RootStoreContext.Provider;
+
+export function useMst() {
+    const store = React.useContext(RootStoreContext);
+    if (store == null) {
+      throw new Error("Store cannot be null, please add a context provider");
+    }
+    return store;
+}
